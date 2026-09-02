b68e452bbbca027b4f8e3b16b1a6256d0cd5476e
aegntic <hello@aegntic.com>
2026-09-02 20:58:44 +1000

    chore: remove irrelevant dirs (02_Showcase/THRESHOLD-INC, brik-pro)

--- 02_Showcase/THRESHOLD-INC/insidher-v1/android-app/src/main/java/com/thresholdinc/insidher/ui/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.thresholdinc.insidher.ui
-
-object Routes {
-    const val WALKTHROUGH = "walkthrough"
-    const val ONBOARDING = "onboarding"
-    const val THREADS = "threads"
-    const val THREAD = "thread/{threadId}"
-    fun thread(id: String) = "thread/$id"
-}
